8288025d99d3fd056495d89105c05c38e3d11932
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-27 19:35:17 -0300

    :lipstick: feat: Responsividade do header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Avatar, Text, Box } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Pedro Vinícius</Text>
-        <Text color="gray.300" fontSize="small">
-          pedrovinicius.dev@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Pedro Vinícius</Text>
+          <Text color="gray.300" fontSize="small">
+            pedrovinicius.dev@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
