598656b8e5f4389cfd9af1423b8e27513d980bcc
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-27 20:30:05 -0300

    :lipstick: feat: Responsividade da sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
-import { Logo } from './Logo';
-import { NotificationsNav } from './NotificationsNav';
-import { Profile } from './Profile';
-import { SearchBox } from './SearchBox';
+import { useSidebarDrawer } from '@/src/contexts/SidebarDrawerContext';
+import { Flex, IconButton, useBreakpointValue, Icon } from "@chakra-ui/react";
+import { RiMenuLine } from 'react-icons/ri';
+import { Logo } from "./Logo";
+import { NotificationsNav } from "./NotificationsNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const {onOpen} = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
+  });
 
   return (
     <Flex
@@ -20,16 +24,25 @@ export function Header() {
       mt="4"
       px="6"
       align="center"
-    > 
-      <Logo />
-      { isWideVersion && <SearchBox /> }
+    >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine}/>}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+        </IconButton>
+      ) }
 
-      <Flex 
-        align="center"
-        ml="auto"
-      >
+      <Logo />
+      {isWideVersion && <SearchBox />}
+
+      <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile showProfileData={isWideVersion}/>
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
