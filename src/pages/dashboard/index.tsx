47bf91645cf1b635831982489eceaa731326eccc
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-27 20:45:48 -0300

    :lipstick: feat: Responsividade do dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -71,7 +71,7 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
@@ -83,7 +83,7 @@ export default function Dashboard() {
               width="100%"
             />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
